8c57037166657465b4a17fe8e198c80cce3e9fba
Erick Hernandez <erickhernandez0492@gmail.com>
2025-05-23 09:42:09 +0200

    feat: create hook useCounter

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'warn', // activa errores de Prettier en ESLint
+    },
+  },
+  prettierConfig, // desactiva reglas de ESLint que chocan con Prettier
+];
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+import './index.css';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <HooksApp /> */}
+    {/* <CounterApp /> */}
+    <CounterWithCustomHook></CounterWithCustomHook>
+  </StrictMode>,
+);
